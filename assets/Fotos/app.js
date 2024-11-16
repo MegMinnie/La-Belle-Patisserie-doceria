b25d151a12f3ffd06f241dfce533fef15740d384
Meg <marinaadasilva3456@gmail.com>
2024-11-16 20:30:31 -0300

    fix: Fiz melhorias

--- assets/Fotos/app.js
+++ assets/Fotos/app.js
@@ -2,6 +2,35 @@ const botaoUm = document.querySelector(".botaoUm");
 const botaoDois = document.querySelector(".botaoDois");
 const botaoTres = document.querySelector(".botaoTres");
 const botaoQuatro = document.querySelector(".botaoQuatro");
+const botoesPc = document.querySelectorAll("#botao-pc");
+
+window.addEventListener("DOMContentLoaded", function () {
+  const primeiroBotao = botoesPc[0];
+  if (primeiroBotao) {
+    primeiroBotao.classList.add(
+      "bg-white",
+      "text-red-500",
+      "p-1",
+      "rounded-[10px]"
+    );
+  }
+});
+
+Array.from(botoesPc).forEach((botaoPc, _, botoesPc) => {
+  botaoPc.addEventListener("click", function () {
+    botoesPc.forEach((botaoPc) => {
+      botaoPc.classList.remove("bg-white");
+      botaoPc.classList.remove("text-red-500");
+      this.classList.remove("p-1");
+      this.classList.add("rounded-[5px]");
+    });
+
+    this.classList.add("bg-white");
+    this.classList.add("text-red-500");
+    this.classList.add("p-1");
+    this.classList.add("rounded-[10px]");
+  });
+});
 
 botaoUm.addEventListener("click", function apertar() {
   botaoUm.style.color = "#ff005c";
